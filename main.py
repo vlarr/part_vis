f720f99a273cba56d703b8ca3d5d0771adb52176
Vladimir Rogozin <rogozin.vv.88@gmail.com>
2019-03-15 22:56:03 +0300

    refactor

--- main.py
+++ main.py
@@ -7,12 +7,8 @@ import numpy as np
 def read_xyz_file(path, first_line_offset=2):
     file = open(path, "r")
     slines = file.readlines()[first_line_offset:]
-    return [[line.split('\t')[0] for line in slines],
-            [[float(coord) for coord in line.split('\t')[1:]] for line in slines]]
-
-
-def write_r_file(path, data):
-    open(path, "w").writelines('\n'.join([str(rv) for rv in data]))
+    return [[line.split()[0] for line in slines],
+            [[float(coord) for coord in line.split()[1:]] for line in slines]]
 
 
 def r(c1, c2):
@@ -28,14 +24,6 @@ def calc_center_of_mass(coords, masses):
     return [c / sum(masses) for c in cm]
 
 
-# def calc_r_array(coords, num_r_point=0):
-#     result = []
-#     for i in range(len(coords)):
-#         if i != num_r_point:
-#             result.append(r(coords[i], coords[num_r_point]))
-#     return sorted(result)
-
-
 def calc_cm_array(coords, masses):
     result = []
     cm = calc_center_of_mass(coords, masses)
@@ -46,17 +34,6 @@ def calc_cm_array(coords, masses):
 
 def gauss(x, sigma, mu):
     return np.exp(-0.5 * ((x - mu) / sigma) ** 2) / (sigma * np.sqrt(2.0 * np.pi))
-
-
-def plot_graph(points):
-    offset = 0
-    for parr in points:
-        plt.plot(parr, [offset] * len(parr), 'bd')
-        offset += 0.5
-
-    axes = plt.gca()
-    axes.set_ylim([-1, offset + 0.5])
-    plt.show()
 
 
 def calc_parr(parr, r_min, r_max, step, sigma):
@@ -71,6 +48,7 @@ def calc_parr(parr, r_min, r_max, step, sigma):
 
     return [x_arr, y_arr]
 
+
 def find_r_min_max(points):
     g_min = 10000.0
     g_max = 0
@@ -78,6 +56,18 @@ def find_r_min_max(points):
         g_min = min(g_min, parr[0])
         g_max = max(g_max, parr[len(parr) - 1])
     return [g_min, g_max]
+
+
+def calc_points(file_names):
+    points = []
+    for filename in file_names:
+        read_result = read_xyz_file(filename)
+        coords = read_result[1]  # координаты частиц [][x,y,z]
+        masses = [masses_lib[mass_id] for mass_id in read_result[0]]  # массы частиц []
+        rarr = calc_cm_array(coords, masses)  # массив расстояний от центра массы до частиц |r_cm - r_i|
+        points.append(rarr)
+    return points
+
 
 ########################################################################################################################
 
@@ -90,20 +80,11 @@ masses_lib = {
 points = []
 
 if __name__ == "__main__":
-    for filename in sys.argv[1:]:
-        read_result = read_xyz_file(filename)
-        coords = read_result[1]  # координаты частиц [][x,y,z]
-        masses = [masses_lib[mass_id] for mass_id in read_result[0]]  # массы частиц []
-        rarr = calc_cm_array(coords, masses)  # массив расстояний от центра массы до частиц |r_cm - r_i|
-        points.append(rarr)
-
-# for filename in ["example1xyz"]:
-#     read_result = read_xyz_file(filename)
-#     coords = read_result[1]
-#     masses = [masses_lib[mass_id] for mass_id in read_result[0]]
-#     rarr = calc_cm_array(coords, masses)
-#     points.append(rarr)
-
+    if len(sys.argv) > 1:
+        points = calc_points(sys.argv[1:])
+    else:
+        # raise SystemExit
+        points = calc_points(["example3.xyz", "example4.xyz"])
 
 c_result = []
 
