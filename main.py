a91e0ae46b2791cdecccd7c6ab1efda89023d02a
Vladimir Rogozin <rogozin.vv.88@gmail.com>
2019-03-16 09:59:54 +0300

    добавлены описания функций

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ def r(c1, c2):
     return math.sqrt(math.pow(c1[0] - c2[0], 2) + math.pow(c1[1] - c2[1], 2) + math.pow(c1[2] - c2[2], 2))
 
 
+#   Возвращает центр масс [x,y,z].
+#   Входные параметры: массив координат [][x,y,z] и массив масс []
 def calc_center_of_mass(coords, masses):
     cm = [0, 0, 0]
     for i in range(len(coords)):
@@ -24,6 +26,8 @@ def calc_center_of_mass(coords, masses):
     return [c / sum(masses) for c in cm]
 
 
+#   Возвращает массив расстояний |r_cm - r_i|.
+#   Входные параметры: массив координат [][x,y,z] и массив масс []
 def calc_cm_array(coords, masses):
     result = []
     cm = calc_center_of_mass(coords, masses)
@@ -36,6 +40,9 @@ def gauss(x, sigma, mu):
     return np.exp(-0.5 * ((x - mu) / sigma) ** 2) / (sigma * np.sqrt(2.0 * np.pi))
 
 
+#   Строит массив точек x, y для отображения [[],[]]
+#   Входные параметры: массив расстояний [], левая и правая граница отображения по x, величина шага для отображения,
+#   сигма для гауссовской функции
 def calc_parr(parr, r_min, r_max, step, sigma):
     r_len = r_max - r_min
     npoints = int(r_len / step) + 1
@@ -50,7 +57,7 @@ def calc_parr(parr, r_min, r_max, step, sigma):
 
 
 def find_r_min_max(points):
-    g_min = 10000.0
+    g_min = 0
     g_max = 0
     for parr in points:
         g_min = min(g_min, parr[0])
@@ -58,6 +65,7 @@ def find_r_min_max(points):
     return [g_min, g_max]
 
 
+#   Вычисляет массив расстояний для каждого заданного в аргументах файла [][].
 def calc_points(file_names):
     points = []
     for filename in file_names:
